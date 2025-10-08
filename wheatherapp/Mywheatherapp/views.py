7f23a8446308741f170b9231ec192ec295f6dcfa
aditigupta217 <aditigupta221177@gmail.com>
2025-10-08 23:26:42 +0530

    day2

--- wheatherapp/Mywheatherapp/views.py
+++ wheatherapp/Mywheatherapp/views.py
@@ -1,5 +1,36 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+import requests
+import datetime
 
-# Create your views here.
 def home(request):
-    return HttpResponse("THis is my server")
+    if 'city' in request.POST:
+        city = request.POST['city']
+    else:
+        city = 'Nagpur'
+
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+    PARAMS = {'q': city, 'appid': '7cea25bc525a55c25e65fb7bf1040959', 'units': 'metric'}
+
+    # Make API request
+    response = requests.get(url, params=PARAMS)
+    data = response.json()
+
+    # Handle errors (like invalid city name)
+    if response.status_code != 200 or 'weather' not in data:
+        description = "City not found"
+        icon = "01d"
+        temp = "-"
+    else:
+        description = data['weather'][0]['description']
+        icon = data['weather'][0]['icon']
+        temp = data['main']['temp']
+
+    day = datetime.date.today()
+
+    return render(request, 'mywheatherapp/index.html', {
+        'description': description,
+        'icon': icon,
+        'temp': temp,
+        'day': day,
+        'city': city
+    })
